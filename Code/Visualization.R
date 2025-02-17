2dd1a654ea5aa51c1a88fb5b99ffc266852b78cf
andrillow <126349975+andrillow@users.noreply.github.com>
2025-02-17 11:17:31 -0500

    Update Visualization.R

--- Code/Visualization.R
+++ Code/Visualization.R
@@ -20,9 +20,12 @@ plot(b2, col=cl)
 # try with more colors
 par(mfrow=c(1,3))
 
+dev.off() # turn off things that are open before opening new stuff
 cl <- colorRampPalette(c("darkorchid3", "cyan", "chartreuse", "chocolate", "darkgoldenrod1")) (1000)
 plot(b2, col=cl)
 cl <- colorRampPalette(c("darkorchid3", "cyan", "chartreuse", "chocolate", "darkgoldenrod1")) (100)
 plot(b2, col=cl)
 cl <- colorRampPalette(c("darkorchid3", "cyan", "chartreuse", "chocolate", "darkgoldenrod1")) (3)
 plot(b2, col=cl)
+
+setwd(C:/Users/andreanocentini/OneDrive - Seminole Tribe Of Florida/Desktop/)
