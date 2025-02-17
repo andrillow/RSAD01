8f80d1aa0382ea98e9c41f39fda61aea07cdd018
andrillow <126349975+andrillow@users.noreply.github.com>
2025-02-17 10:59:58 -0500

    Update Visualization.R

--- Code/Visualization.R
+++ Code/Visualization.R
@@ -3,9 +3,10 @@
 library(terra)
 library(imageRy)
 
-# importing data from ImageRy
+# importing data from ImageRy (Sentibel bands)
 im.list()
 
 b2<- im.import("sentinel.dolomites.b2.tif")
 
-# Sentinel bands
+cl <- colorRampPalette(c("cyan", "chocolate", "coral")) (75)
+plot(b2, col=cl)
