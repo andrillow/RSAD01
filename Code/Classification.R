3a3c321086a0cae17d142f996c4c6bd5241a8f99
andrillow <126349975+andrillow@users.noreply.github.com>
2025-02-19 05:24:15 -0500

    Update Classification.R

--- Code/Classification.R
+++ Code/Classification.R
@@ -55,10 +55,21 @@ mato_grosso_df <- data.frame(cover, perc1992, perc2006)
 
 # plot the relative cover by class
 cover_1992_plot <- ggplot(mato_grosso_df, aes(x=cover, y=perc1992, color=cover)) +
-                   geom_bar(stat="identity", fill="white")
+                   geom_bar(stat="identity", fill="white") +
+                   ylim (0, 100)
 cover_1992_plot
 cover_2006_plot <- ggplot(mato_grosso_df, aes(x=cover, y=perc2006, color=cover)) +
-                   geom_bar(stat="identity", fill="white")
+                   geom_bar(stat="identity", fill="white")  +
+                   ylim (0, 100)
 cover_2006_plot
 cover_1992_plot + cover_2006_plot
 
+# plot using the imageRy package
+im.list()
+mato1992 <- im.import("matogrosso_l5_1992219.lrg.jpg")
+mato1992 <- im.ggplot(mato1992, 1)
+mato2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+mato2006 <- im.ggplot(mato2006, 1)
+Composite_plot <- (cover_1992_plot | cover_2006_plot) /
+                  (mato1992 | mato2006)
+Composite_plot
