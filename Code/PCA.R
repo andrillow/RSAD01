d84c8fc7de257dc1618989098b4b4b3c0599ee4c
andrillow <126349975+andrillow@users.noreply.github.com>
2025-02-19 10:34:05 -0500

    Update PCA.R

--- Code/PCA.R
+++ Code/PCA.R
@@ -1,1 +1,14 @@
+# Multivariate analysis in R with satellite data
 
+library(terra)
+library(imageRy)
+
+im.list()
+Sentinel <- im.import("sentinel.png")
+Sentinel <- c(Sentinel[[1]], Sentinel[[2]], Sentinel[[3]])
+
+# Principal Components Analysis (PCA)
+sample <- spatSample(Sentinel, 100) # sample 100 points from the image
+sample
+Sentinel_PCA <- prcomp (sample) # perform the PCA
+summary(Sentinel_PCA)
