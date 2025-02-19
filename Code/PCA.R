0f6e9f0b5a83db9557115e18b59187f0154fc2a7
andrillow <126349975+andrillow@users.noreply.github.com>
2025-02-19 10:43:32 -0500

    Update PCA.R

--- Code/PCA.R
+++ Code/PCA.R
@@ -12,3 +12,23 @@ sample <- spatSample(Sentinel, 100) # sample 100 points from the image
 sample
 Sentinel_PCA <- prcomp (sample) # perform the PCA
 summary(Sentinel_PCA)
+PCA_predicted_map <- predict(Sentinel, Sentinel_PCA, index=c(1:3))
+plot(PCA_predicted_map)
+
+# PCA with a different dataset
+Dolomites <- im.import("sentinel.dolomites")
+sample <- spatSample(Dolomites, 100) # sample 100 points from the image
+sample
+Dolomites_PCA <- prcomp (sample) # perform the PCA
+summary(Dolomites_PCA)
+PCA_predicted_map <- predict(Dolomites, Dolomites_PCA, index=c(1:4))
+plot(PCA_predicted_map)
+
+# PCA with a higher number of sample points
+Dolomites <- im.import("sentinel.dolomites")
+sample <- spatSample(Dolomites, 1000) # sample 100 points from the image
+sample
+Dolomites_PCA <- prcomp (sample) # perform the PCA
+summary(Dolomites_PCA)
+PCA_predicted_map2 <- predict(Dolomites, Dolomites_PCA, index=c(1:4))
+plot(PCA_predicted_map2)
