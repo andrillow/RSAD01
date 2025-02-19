d75cf3257613ecceb793edbd81ca033503d93501
andrillow <126349975+andrillow@users.noreply.github.com>
2025-02-19 11:07:07 -0500

    Update PCA.R

--- Code/PCA.R
+++ Code/PCA.R
@@ -15,16 +15,7 @@ summary(Sentinel_PCA)
 PCA_predicted_map <- predict(Sentinel, Sentinel_PCA, index=c(1:3))
 plot(PCA_predicted_map)
 
-# PCA with a different dataset
-Dolomites <- im.import("sentinel.dolomites")
-sample <- spatSample(Dolomites, 100) # sample 100 points from the image
-sample
-Dolomites_PCA <- prcomp (sample) # perform the PCA
-summary(Dolomites_PCA)
-PCA_predicted_map <- predict(Dolomites, Dolomites_PCA, index=c(1:4))
-plot(PCA_predicted_map)
-
-# PCA with a higher number of sample points
+# PCA with a different dataset, sampling a higher number of points
 Dolomites <- im.import("sentinel.dolomites")
 sample <- spatSample(Dolomites, 1000) # sample 100 points from the image
 sample
@@ -32,3 +23,13 @@ Dolomites_PCA <- prcomp (sample) # perform the PCA
 summary(Dolomites_PCA)
 PCA_predicted_map2 <- predict(Dolomites, Dolomites_PCA, index=c(1:4))
 plot(PCA_predicted_map2)
+
+# PCA using the imageRy package
+Sentinel_PCA_im <- im.pca(Sentinel)
+Dolomites_PCA_im <- im.pca(Dolomites, 100, 4) # setting number of samples and number of components to map
+
+# calcualting variability of the first principal component
+var_Sentinel_PCA_im <- focal(Sentinel_PCA_im[[1]], matrix(1/9,3,3), fun=sd)
+var_Dolomites_PCA_im <- focal(Dolomites_PCA_im[[1]], matrix(1/9,3,3), fun=sd)
+var_Sentinel_PCA_im_p <- im.ggplot(var_Sentinel_PCA_im)
+var_Dolomites_PCA_im_p <- im.ggplot(var_Dolomites_PCA_im)
