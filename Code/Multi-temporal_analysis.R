cbb465989ac4ba978ec1e62c53883c8be592d521
andrillow <126349975+andrillow@users.noreply.github.com>
2025-02-20 06:13:41 -0500

    Update Multi-temporal_analysis.R

--- Code/Multi-temporal_analysis.R
+++ Code/Multi-temporal_analysis.R
@@ -1,1 +1,15 @@
+# Time series analysis
 
+library(terra)
+library(imageRy)
+library(ggplot2)
+library(patchwork)
+
+im.list()
+EN01 <- im.import("EN_01.png")
+EN01 <- flip(EN01)
+EN13 <- im.import("EN_13.png")
+EN13 <- flip(EN13)
+EN01RGB <- ggRGB(EN01, 1, 2, 3)
+EN13RGB <- ggRGB(EN13, 1, 2, 3)
+EN01RGB / EN13RGB
