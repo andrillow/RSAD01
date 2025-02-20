526fb95b4cccf6b8707bab2d4e51190242bab430
andrillow <126349975+andrillow@users.noreply.github.com>
2025-02-20 10:55:16 -0500

    Update Hexagon_binning.R

--- Code/Hexagon_binning.R
+++ Code/Hexagon_binning.R
@@ -1,1 +1,28 @@
+# Plotting data in scatter plots using hexagon binning
 
+library(terra)
+library(imageRy)
+library(hexbin)
+library(ggplot2)
+
+im.list()
+Dolomites <- im.import("sentinel.dolomites")
+plot(Dolomites[[1]], Dolomites[[2]])
+pairs(Dolomites)
+
+# hexagon binning
+NDVI <- im.import("_NDVI_20")
+par(mfrow=c(3,1))
+plot(NDVI[[2]])
+plot(NDVI[[3]])
+plot(NDVI[[2]], NDVI[[3]], xlim=c(-0.4, 0.8), ylim=c(-0.4, 0.8), xlab="May", ylab="August")
+abline(0, 1, col="red")
+dev.off()
+NDVI_may <- as.data.frame(NDVI[[2]])
+NDVI_august <- as.data.frame(NDVI[[3]])
+NDVI_HB <- hexbin(NDVI_may[[1]], NDVI_august[[1]], xbins=100)
+plot(NDVI_HB)
+NDVI_HB2 <- ggplot(data.frame(x=NDVI_may[[1]], y=NDVI_august[[1]]), aes(x=x, y=y)) +
+            geom_hex() +
+            scale_fill_viridis(option="mako")
+NDVI_HB2  # 
